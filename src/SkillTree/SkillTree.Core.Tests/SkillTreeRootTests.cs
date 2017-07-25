8cc954bdd40fa91f70e742f2f8d5d9611e49085f
Wojciech Malara <malara.wojciech@gmail.com>
2017-07-25 22:17:39 +0200

    Added basic SkillTreeRoot class

---
+++ src/SkillTree/SkillTree.Core.Tests/SkillTreeRootTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace SkillTree.Core.Tests
+{
+    public class SkillTreeRootTests
+    {
+        [Fact]
+        public void CanAddSkills()
+        {
+            // Arrange
+            var root = new SkillTreeRoot();
+            var skill1 = Skill.Create("Programming");
+            var skill2 = Skill.Create("Economics");
+
+            // Act
+            root.AddSkill(skill1);
+            root.AddSkill(skill2);
+
+            // Assert
+            Assert.True(root.Skills.Contains(skill1));
+            Assert.True(root.Skills.Contains(skill2));
+        }
+    }
+}
---
+++ src/SkillTree/SkillTree.Core/SkillTreeRoot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillTree.Core
+{
+    public class SkillTreeRoot
+    {
+        private readonly List<Skill> skills;
+
+        public SkillTreeRoot()
+        {
+            skills = new List<Skill>();
+        }
+
+        public IReadOnlyList<Skill> Skills => skills;
+
+        public void AddSkill(Skill skill)
+        {
+            skills.Add(skill);
+        }
+    }
+}
